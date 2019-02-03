3735479f00d8e261d852759feb0f42b71f89e5f8
Vyacheslav Bobrov <bobrov.vy@gmail.com>
2019-02-03 14:10:39 +0300

    Добавил заготовку springboot, для приложения

---
+++ src/main/java/ru/bobrov/vyacheslav/comsole_chat/ComsoleChatApplication.java
@@ -0,0 +1,14 @@
+package ru.bobrov.vyacheslav.comsole_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComsoleChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComsoleChatApplication.class, args);
+	}
+
+}
+
